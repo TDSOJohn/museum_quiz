f77430a88eedc9e469c69199d32a50b6ddb10734
TDSOJohn <giovanni.basso@icloud.com>
2020-06-24 16:43:52 +0200

    client_side js function

---
+++ client_side/client_side.js
@@ -0,0 +1,19 @@
+//  Using the (new) Fetch API and even newer async() function type
+//  Support: Chrome 55+, Edge 15+, Firefox 52+, Opera 42+, Safari 10.1+ (no IE support)
+//  If not supported, use XMLHttpRequest instead
+
+const baseURL   = '192.168.1.110';
+const apiPort   = '3000';
+var id          = '1';
+
+const callAPI   = async () =>
+{
+//  Build well-formed WHATWG URL and wait for json data
+    let myURL       = 'http://' + baseURL + ':' + apiPort + '/&id=' + id;
+    alert(myURL);
+
+    const response  = await fetch(myURL);
+    const apiJSON   = await response.json();
+}
+
+callAPI();
