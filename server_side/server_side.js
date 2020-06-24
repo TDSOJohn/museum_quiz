70f010e433033da4e597817ea2dd7007cf75d29d
TDSOJohn <giovanni.basso@icloud.com>
2020-06-24 19:23:00 +0200

    trying to implement html server

--- server_side/server_side.js
+++ server_side/server_side.js
@@ -1,3 +1,5 @@
+//  RESTful API URL format: http://192.168.1.110/?id=n (n is integer {1, 2, 3})
+//  client-side HTML retrieval URL format: http://192.168.1.110/client_side/index.php
 //  javascript is in strict mode => throws more errors and could give better performance
 'use strict';
 
@@ -17,40 +19,58 @@ const server    = http.createServer((request, response) =>
 //  myURL is (baseURL + request.url) in order to form a correct WHATWG URL
     var baseURL = 'http://' + request.headers.host;
     var myURL   = new URL(request.url, baseURL);
+    var pathName= myURL.pathname;
     var id      = myURL.searchParams.get('id');
+    console.log(pathName);
     console.log(id);
 
+    if(pathName === '/client_side')
+    {
+        try
+        {
+            let htmlPathName    = 'client_side/basic.html';
+            let htmlData        = fs.readFileSync(htmlPathName);
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'text/html');
+            response.write(htmlData);
+        } catch (e)
+        {
+
+        }
+    }
+
+//  RESTful API part
 //  readFileSync imposes program halting till file is read
 //  Error handling if file is missing (ENOENT) or access is not permitted (EACCES), otherwise throw err
 //  If error in syscall is not handled, everything will crash <3
-    try
-    {
+        try
+        {
 //  Build jsonPathName from id and try to readSync it
-        let jsonPathName    = `data${id}.json`;
-        let rawData         = fs.readFileSync(jsonPathName);
+            let jsonPathName    = `data${id}.json`;
+            let rawData         = fs.readFileSync(jsonPathName);
 
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'application/json');
-        response.write(rawData);
-    } catch(err)
-    {
-        if(err.code === 'ENOENT')
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            response.write(rawData);
+        } catch(err)
         {
-            response.statusCode = 404;
-            response.setHeader('Content-Type', 'text/plain');
-            response.write('404 - file not found!');
+            if(err.code === 'ENOENT')
+            {
+                response.statusCode = 404;
+                response.setHeader('Content-Type', 'text/plain');
+                response.write('404 - file not found!');
+            }
+            else if(err.code === 'EACCES')
+            {
+                response.statusCode = 403;
+                response.setHeader('Content-Type', 'text/plain');
+                response.write('403 - file reading permission denied!');
+            }
+            else
+            {
+                throw err;
+            }
         }
-        else if(err.code === 'EACCES')
-        {
-            response.statusCode = 403;
-            response.setHeader('Content-Type', 'text/plain');
-            response.write('403 - file reading permission denied!');
-        }
-        else
-        {
-            throw err;
-        }
-    }
     response.end();
 });
 
