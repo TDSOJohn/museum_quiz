9d81f05bf779cb70c7ce4f3d96ff30f27399420e
System Administrator <root@Giovannis-MacBook-Pro.local>
2020-06-30 18:09:36 +0200

    added terrible Access-Control-Allow_Origin wildcard

--- client_side/client_side.js
+++ client_side/client_side.js
@@ -2,9 +2,36 @@
 //  Support: Chrome 55+, Edge 15+, Firefox 52+, Opera 42+, Safari 10.1+ (no IE support)
 //  If not supported, use XMLHttpRequest instead
 
-const baseURL   = '192.168.1.110';
+//const baseURL   = '192.168.1.110';
+const baseURL   = '127.0.0.1';
 const apiPort   = '3000';
 var id          = '1';
+
+//  Sanitizes string_in checking for ints. Warning: 11fxoifS => 11, a11b => NaN
+function intParser(string_in)
+{
+    const parsed = parseInt(string_in, 10);
+    if(isNaN(parsed) && (parsed !== null))
+    {
+        return -1;
+    }
+    return parsed;
+}
+
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++)
+    {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable)
+        {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
 
 const callAPI   = async () =>
 {
@@ -17,7 +44,11 @@ const callAPI   = async () =>
     const response  = await fetch(myURL);
     const apiJSON   = await response.json();
 
-    document.getElementById("this_p").innerHTML = apiJSON;
+    const hrJSON    = JSON.stringify(apiJSON);
+
+    document.getElementById("this_p").innerHTML = hrJSON;
 }
+
+id = intParser(getQueryVariable('id'));
 
 callAPI();
