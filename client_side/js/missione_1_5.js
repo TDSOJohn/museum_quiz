2a827729c6b2fb1ae89b46340bef124b453478c2
micolll <48279072+micolll@users.noreply.github.com>
2020-09-18 11:39:42 +0200

    Add files via upload

---
+++ client_side/js/missione_1_5.js
@@ -0,0 +1,43 @@
+var risp = [{
+        'testo': '1888',
+        'giusta': true
+    },
+    {
+        'testo': '1900',
+        'giusta': false
+    },
+    {
+        'testo': '1780',
+        'giusta': false
+    }
+];
+
+window.onload = function() {
+    console.log("loaded!");
+    rbs = document.querySelectorAll("label");
+    for (let i = 0; i < rbs.length; ++i) {
+        rbs[i].innerHTML = "<input type=\"radio\" name=\"optradio\" checked>" + risp[i].testo;
+    }
+};
+
+//  answer-checking function
+function evaluate_answer() {
+    let selectedValue;
+    for (let i = 0; i < risp.length; i++) {
+        if (risp[i].giusta) {
+            selectedValue = risp[i].testo;
+            break;
+        }
+    }
+    var gg;
+    for (let i = 0; i < risp.length; ++i) {
+        if (risp[i].testo == selectedValue) {
+            gg = risp[i].giusta;
+        }
+    }
+    gg == true ? alert("Missione compiuta") :
+        alert("Che peccato hai sbagliato! Ritenta, questa volta non sbaglierai!");
+
+
+
+};
