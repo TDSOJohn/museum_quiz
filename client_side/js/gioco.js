6802dce7e205e7d36fd49ae484ae015951118ef6
TDSOJohn <giovanni.basso@icloud.com>
2020-10-29 12:29:02 +0100

    updated json

--- client_side/js/gioco.js
+++ client_side/js/gioco.js
@@ -23,7 +23,7 @@ const callAPI   = async () =>
     alert(myURL);
 
     const response  = await fetch(myURL);
-    file            = await response;
+    file            = await response.json();
     MyArr = file;
     alert(file);
 }
