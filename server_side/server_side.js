154c54dad37de56aee2568f606dc484db71ff2dd
TDSOJohn <giovanni.basso@icloud.com>
2021-01-16 15:01:10 +0100

    bugfixing

--- server_side/server_side.js
+++ server_side/server_side.js
@@ -137,7 +137,7 @@ const server = http.createServer((request, response) => {
     //  Then send a 303 - See Other with a location header to newly created qr code
     else if (method == 'POST')
     {
-        console.log(`${request.getHeader('Content-Type')}`);
+        console.log(JSON.stringify(req.headers));
         let body = [];
         let dbFiles = undefined;
         let qrFilePath = undefined;
